29c0e63151ccdd1cd9067e5ddd56c1631e311762
bzndenis <bzndenis@gmail.com>
2022-12-21 15:42:53 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+//$config['base_url'] = 'https://spk.d-undangan.buzz/';
 $config['base_url'] = 'http://localhost:3000/';
+
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,25 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'dundang1_wp_kinerja',
+// 	'password' => 'q1w2e3qwe123',
+// 	'database' => 'dundang1_wp_kinerja',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
